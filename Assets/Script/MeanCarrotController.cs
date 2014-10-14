91751f0ca7b44dddbce41ccc3b3bd2d34e689671
unknown <s109-student@S109-03.deltacampus.net>
2014-10-14 15:41:35 -0700

    update mean carrot controll scrips

--- Assets/Script/MeanCarrotController.cs
+++ Assets/Script/MeanCarrotController.cs
@@ -17,6 +17,12 @@ public class MeanCarrotController : MonoBehaviour{
 	float travelDirectionTimer;
 	float travelTime = 0.0f;
 
+
+	const float distanceFromHero = 2;
+
+
+ 	//GameObject theBoy = GameObject.Find("boy");
+
 	// Use this for initialization
 	void Start () {
 		moveDirection = Vector3.right + Vector3.down;
@@ -29,9 +35,9 @@ public class MeanCarrotController : MonoBehaviour{
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 currentPosition = transform.position;
+				Vector3 currentPosition = transform.position;
 
-		/* // going to use this to code projectiles that will come out of this enemy and flies toward the main character
+				/* // going to use this to code projectiles that will come out of this enemy and flies toward the main character
 		if (Input.GetButton ("Fire1")) {
 			Vector3 moveToward = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			moveDirection = moveToward - currentPosition;
@@ -40,68 +46,69 @@ public class MeanCarrotController : MonoBehaviour{
 		}
 		*/
 
-		Vector3 target = moveDirection * moveSpeed + currentPosition;
-		transform.position = Vector3.Lerp (currentPosition, target, Time.deltaTime);
+				Vector3 target = moveDirection * moveSpeed + currentPosition;
+				transform.position = Vector3.Lerp (currentPosition, target, Time.deltaTime);
 
-		if( (travelTime += Time.deltaTime) >= travelDirectionTimer ){
-			SpriteAnimation.isStandingStill = false;
-			int whichDirection = (int)Random.Range(0, 4);
 
-			if( (whichDirection != 4) && ((++travelLimitorArray[whichDirection]) == travelSameDirLimitor) ){
-				int anotherDirection;
-				do{
-					anotherDirection = (int)Random.Range(0, 4);
-				}while(anotherDirection == whichDirection);
+				// how long sprite will move in a given direction
+				if ((travelTime += Time.deltaTime) >= travelDirectionTimer) {
+						SpriteAnimation.isStandingStill = false;
+						int whichDirection = (int)Random.Range (0, 4);
 
-				whichDirection = anotherDirection;
-				for(int i = 0; i < travelLimitorArray.Length; ++i){
-					travelLimitorArray[i] = 0;
+						if ((whichDirection != 4) && ((++travelLimitorArray [whichDirection]) == travelSameDirLimitor)) {
+								int anotherDirection;
+								do {
+										anotherDirection = (int)Random.Range (0, 4);
+								} while(anotherDirection == whichDirection);
+
+								whichDirection = anotherDirection;
+								for (int i = 0; i < travelLimitorArray.Length; ++i) {
+										travelLimitorArray [i] = 0;
+								}
+								++travelLimitorArray [whichDirection];
+						}
+
+						for (int i = 0; i < travelLimitorArray.Length; ++i) {
+								if (i == whichDirection)
+										continue;
+								travelLimitorArray [i] = 0;
+						}
+
+						if (whichDirection == (int)SpriteAnimation.travelDirection.RIGHT_UP) {
+								if (!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.RIGHT_UP)) {
+										moveDirection = Vector3.right + Vector3.up;
+										SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.RIGHT_UP;
+										SpriteAnimation.frameOffset = SpriteAnimation.offset.RIGHT_UP;
+								}
+						} else if (whichDirection == (int)SpriteAnimation.travelDirection.LEFT_UP) {
+								if (!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.LEFT_UP)) {
+										moveDirection = Vector3.left + Vector3.up;
+										SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.LEFT_UP;
+										SpriteAnimation.frameOffset = SpriteAnimation.offset.LEFT_UP;
+								}
+						} else if (whichDirection == (int)SpriteAnimation.travelDirection.RIGHT_DOWN) {
+								if (!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.RIGHT_DOWN)) {
+										moveDirection = Vector3.right + Vector3.down;
+										SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.RIGHT_DOWN;
+										SpriteAnimation.frameOffset = SpriteAnimation.offset.RIGHT_DOWN;
+								}
+						} else if (whichDirection == (int)SpriteAnimation.travelDirection.LEFT_DOWN) {
+								if (!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.LEFT_DOWN)) {
+										moveDirection = Vector3.left + Vector3.down;
+										SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.LEFT_DOWN;
+										SpriteAnimation.frameOffset = SpriteAnimation.offset.LEFT_DOWN;
+								}
+						} else { // standstill
+								SpriteAnimation.isStandingStill = true;
+						}
+
+						travelTime = 0.0f;
+						travelDirectionTimer = Random.Range (minimumSecond, maximumSecond);
 				}
-				++travelLimitorArray[whichDirection];
-			}
+		Vector3 carrotLoc = transform.position;
+		Vector3 boyLoc = GameObject.Find("boy").transform.position;
+		Debug.Log(Vector3.Distance(carrotLoc, boyLoc));
 
-			for(int i = 0; i < travelLimitorArray.Length; ++i){
-				if(i == whichDirection)
-					continue;
-				travelLimitorArray[i] = 0;
-			}
-
-			if(whichDirection == (int)SpriteAnimation.travelDirection.RIGHT_UP){
-				if(!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.RIGHT_UP)){
-					moveDirection = Vector3.right + Vector3.up;
-					SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.RIGHT_UP;
-					SpriteAnimation.frameOffset = SpriteAnimation.offset.RIGHT_UP;
-				}
-			}
-			else if(whichDirection == (int)SpriteAnimation.travelDirection.LEFT_UP){
-				if(!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.LEFT_UP)){
-					moveDirection = Vector3.left + Vector3.up;
-					SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.LEFT_UP;
-					SpriteAnimation.frameOffset = SpriteAnimation.offset.LEFT_UP;
-				}
-			}
-			else if(whichDirection == (int)SpriteAnimation.travelDirection.RIGHT_DOWN){
-				if(!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.RIGHT_DOWN)){
-					moveDirection = Vector3.right + Vector3.down;
-					SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.RIGHT_DOWN;
-					SpriteAnimation.frameOffset = SpriteAnimation.offset.RIGHT_DOWN;
-				}
-			}
-			else if(whichDirection == (int)SpriteAnimation.travelDirection.LEFT_DOWN){
-				if(!(SpriteAnimation.currentTravelDirection == SpriteAnimation.travelDirection.LEFT_DOWN)){
-					moveDirection = Vector3.left + Vector3.down;
-					SpriteAnimation.currentTravelDirection = SpriteAnimation.travelDirection.LEFT_DOWN;
-					SpriteAnimation.frameOffset = SpriteAnimation.offset.LEFT_DOWN;
-				}
-			}
-			else{ // standstill
-				SpriteAnimation.isStandingStill = true;
-			}
-
-			travelTime = 0.0f;
-			travelDirectionTimer = Random.Range(minimumSecond, maximumSecond);
-		}
-	
 		//float targetAngle = Mathf.Atan2 (moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
 		//transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, targetAngle), turnSpeed * Time.deltaTime);
 	}
